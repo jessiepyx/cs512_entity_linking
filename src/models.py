7e4e45cd7a768f705f6b333609b5d65394a8ce06
Jessie Peng <jessie_p@yeah.net>
2020-05-03 21:01:29 -0500

    PriorModel

--- src/models.py
+++ src/models.py
@@ -15,3 +15,16 @@ class RandomModel:
             pred_cids.append(random.choice(mention.candidates).id if mention.candidates else 'NIL')
         return pred_cids
 
+
+class PriorModel:
+    def __init__(self):
+        pass
+
+    def fit(self, dataset):
+        pass
+
+    def predict(self, dataset):
+        pred_cids = []
+        for mention in dataset.mentions:
+            pred_cids.append(max(mention.candidates, key=lambda x: x.prob).id if mention.candidates else 'NIL')
+        return pred_cids
